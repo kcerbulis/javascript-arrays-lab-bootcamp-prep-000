401a88fdfb802dc5816fcecd5c46474249d5203e
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-06 23:48:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+
+
+var kit = new Array()
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function removeFirstKitten(kit){
+  kit = kittens.shift()
+  kittens.unshift("Milo")
+  return kit
+}
